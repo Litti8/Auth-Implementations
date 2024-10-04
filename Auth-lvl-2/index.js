9dfc97b29f5e75651af6c4e4a1aee085b87be3b7
Lichatti <lisandropiatti@gmail.com>
2024-10-04 18:35:36 +0200

    Auth-lvl-2/update

--- Auth-lvl-2/index.js
+++ Auth-lvl-2/index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+import bcrypt from "bcrypt";
 
 const app = express();
 const port = 3000;
+const saltRounds = 10;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
